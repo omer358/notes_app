e992a624a44cfa12d679449c933e38d37a86e575
omer358 <omermaki358@gmail.com>
2023-02-24 20:05:10 +0200

    support Material3

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:notes_app/screens/home_screen.dart';
+import 'package:notes_app/style/color_schemes.g.dart';
 
 void main(List<String> args) {
   runApp(const MyApp());
@@ -13,6 +14,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: "My app",
+        theme: ThemeData(useMaterial3: true, colorScheme: lightColorScheme),
+        darkTheme: ThemeData(useMaterial3: true, colorScheme: darkColorScheme),
       home:  NotesPage()
     );
   }
