a37885e21c962a1297fb5626193a3769312856d9
omer358 <omermaki358@gmail.com>
2023-03-03 19:46:29 +0200

    add editing note feature

--- lib/controllers/notes_controller.dart
+++ lib/controllers/notes_controller.dart
@@ -3,12 +3,13 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:notes_app/models/note_model.dart';
+import 'package:notes_app/screens/home_screen.dart';
 import 'package:notes_app/services/database_helper.dart';
 
 class NotesController extends GetxController{
   Rx<List<Note>> notesList = Rx<List<Note>>([]);
-  final titleController = TextEditingController();
-  final contentController = TextEditingController();
+  var titleController = TextEditingController();
+  var contentController = TextEditingController();
 
   @override
   void onInit() {
@@ -43,7 +44,19 @@ class NotesController extends GetxController{
     getAllNotes();
     Get.back();
   }
-
+  void updateNote(int? id) async {
+    String title = titleController.text;
+    String content = contentController.text;
+    Note note = Note(
+      id: id,
+      title: title,
+      content: content,
+    );
+    var updatedNote = await DatabaseHelper.updateNote(note);
+    log("$updatedNote");
+    getAllNotes();
+    Get.offAll(()=>NotesPage());
+  }
   void deleteNote(int? noteId) async{
     await DatabaseHelper.deleteNote(noteId!);
     getAllNotes();
