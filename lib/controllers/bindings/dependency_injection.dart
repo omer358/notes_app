6af67abbf6a1cfd4288b51ff7322f4ba83b5579b
omer358 <omermaki358@gmail.com>
2023-10-10 03:21:50 +0200

    bind GetConnect

--- lib/controllers/bindings/dependency_injection.dart
+++ lib/controllers/bindings/dependency_injection.dart
@@ -1,7 +1,9 @@
 import 'package:get/get.dart';
+import 'package:notes_app/services/network_calls.dart';
 
 class DependencyInjection {
   static void init() async {
     Get.put<GetConnect>(GetConnect());
+    Get.put<RestAPI>(RestAPI());
   }
 }
