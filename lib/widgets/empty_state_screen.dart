00ce2cba12947e340861a9d8958bd3841650b509
omer358 <omermaki358@gmail.com>
2023-10-19 22:45:29 +0200

    center the empty state screen

---
+++ lib/widgets/empty_state_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
+
+class EmptyState extends StatelessWidget {
+  const EmptyState({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Lottie.asset('assets/images/empty.json'),
+          Text(
+            "empty_state".tr,
+            style: const TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+          ),
+        ],
+      ),
+    );
+  }
+}
